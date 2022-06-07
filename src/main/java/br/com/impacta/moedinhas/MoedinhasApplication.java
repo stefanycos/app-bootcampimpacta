7a0aefa8e1a3a8fc136fd7f5162e7c6dde2397d9
Stefany Souza <stefanysouza@MacBook-Air-de-Stefany.local>
2022-06-07 18:15:44 -0300

    Spring Boot initial structure

---
+++ src/main/java/br/com/impacta/moedinhas/MoedinhasApplication.java
@@ -0,0 +1,13 @@
+package br.com.impacta.moedinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoedinhasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoedinhasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/impacta/moedinhas/MoedinhasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.impacta.moedinhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoedinhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
